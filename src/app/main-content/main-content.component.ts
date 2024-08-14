a65b021e8cc7828b4255556462f43f28589fe1eb
FabianFaber <fabian-faber@hotmail.de>
2024-08-14 07:08:41 +0200

    added animations

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ContactComponent } from './contact/contact.component';
+import  AOS  from 'aos';
 
 @Component({
   selector: 'app-main-content',
@@ -12,6 +13,9 @@ import { ContactComponent } from './contact/contact.component';
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
-export class MainContentComponent {
+export class MainContentComponent  implements OnInit{
+  ngOnInit(){
+    AOS.init();
+  }
 
 }
